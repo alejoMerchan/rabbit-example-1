5150ee66680a217fe942d161a28cf8a36f7be304
abelmeos <amerchan@sura.com.co>
2017-02-13 17:11:58 -0500

    - actualizacion implementacion rabbitmq con libreria op-rabbit

--- build.sbt
+++ build.sbt
@@ -4,9 +4,16 @@ version := "1.0"
 
 scalaVersion := "2.11.7"
 
+resolvers ++= Seq(
+  "SpinGo OSS" at "http://spingo-oss.s3.amazonaws.com/repositories/releases"
+)
+
+val opRabbitVersion = "1.6.0"
+
+
 libraryDependencies ++= Seq(
   "com.typesafe.slick" %% "slick" % "3.1.1",
-  "org.slf4j" % "slf4j-nop" % "1.6.4",
+  "org.slf4j" % "slf4j-api" % "1.7.21",
   "org.suecarter" %% "freeslick" % "3.1.1.0" withSources(),
   "com.typesafe.slick" %% "slick-hikaricp" % "3.1.0",
   "com.typesafe.akka" %% "akka-http" % "10.0.0",
@@ -17,6 +24,10 @@ libraryDependencies ++= Seq(
   "org.scalaz" %% "scalaz-core" % "7.1.0",
   "com.chuusai" %% "shapeless"  % "2.0.0" withSources(),
   "org.scalatest"  %% "scalatest" % "2.2.1" % "test",
-  "com.thenewmotion" %% "akka-rabbitmq" % "3.0.0"
+  "com.spingo" %% "op-rabbit-core"        % opRabbitVersion,
+  "com.spingo" %% "op-rabbit-play-json"   % opRabbitVersion,
+  "com.spingo" %% "op-rabbit-json4s"      % opRabbitVersion,
+  "com.spingo" %% "op-rabbit-airbrake"    % opRabbitVersion,
+  "com.spingo" %% "op-rabbit-akka-stream" % opRabbitVersion
 )
     
--- src/main/scala/com/example/rabbit/PublishSubscribeExample.scala
+++ src/main/scala/com/example/rabbit/PublishSubscribeExample.scala
@@ -1,19 +1,52 @@
 package com.example.rabbit
 
-import akka.actor.ActorSystem
+import akka.actor.{Props, ActorSystem}
 import com.rabbitmq.client.ConnectionFactory
+import com.spingo.op_rabbit._
 import com.thenewmotion.akka.rabbitmq.ConnectionActor
+import  play.api.libs.json._
+
+import scala.concurrent.ExecutionContext
+
 
 /**
  * Created by abelmeos on 2017/02/13.
  */
-class PublishSubscribeExample extends App{
 
-  implicit val system = ActorSystem()
-  val factory = new ConnectionFactory
-  val connection = system.actorOf(ConnectionActor.props(factory), "rabbitmq")
+case class Data(id: Int)
 
-  val exchange = "amq.fanout"
+object PublishSubscribeExample extends App{
+
+  import PlayJsonSupport._
+
+  implicit val dataFormat = Json.format[Data]
+
+  implicit val actorSystem = ActorSystem("such-system")
+  val rabbitControl = actorSystem.actorOf(Props[RabbitControl])
+  implicit val recoveryStrategy = RecoveryStrategy.nack(false)
+
+  import ExecutionContext.Implicits.global
+
+  val demoQueue = Queue("demo", durable = true, autoDelete = true)
+
+
+
+  val suscription = Subscription.run(rabbitControl){
+    import Directives._
+    channel(qos= 3){
+      consume(demoQueue){
+        body(as[Data]){
+
+          data=>
+            println(data)
+            ack
+
+        }
+      }
+    }
+  }
+
+
 
 
 }
